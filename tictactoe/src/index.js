00f8111a8d858e055aa3fc7c5f344a50f4e4a3a2
Thiago Manzi <thiago_manzi@hotmail.com>
2023-07-13 18:02:00 -0300

    feat(8): board now is able to tell who's turn it is

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -15,13 +15,14 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick = (i) => {
         const squares = this.state.squares.slice();
-        squares[i] = "X";
-        this.setState({ squares });
+        squares[i] = this.state.xIsNext ? "X" : "O";
+        this.setState({ squares, xIsNext: !this.state.xIsNext });
     };
 
     renderSquare(i) {
@@ -31,7 +32,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = "Next player: X";
+        const status = "Next player: " + (this.state.xIsNext ? "'X'" : "'O'");
         return (
             <div>
                 <div className="status">{status}</div>
